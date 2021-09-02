c370852572a03f3893ef6f1181dc5ae48b88270b
Andrea Kristo <andrea.kristo@emovis.hr>
2021-09-02 18:20:58 +0200

    dProject - Minor fix

--- Controllers/TrailerController.cs
+++ Controllers/TrailerController.cs
@@ -167,7 +167,7 @@ namespace dProject.Controllers
 
             var data = await apiLib.SearchTitleAsync(searchText);
 
-            if (data != null && data.Results != null)
+            if (data != null && data.Results != null && data.Results.Count != null)
             {
                 string id = data.Results.FirstOrDefault().Id;
                 trailer = await apiLib.TrailerAsync(id);
